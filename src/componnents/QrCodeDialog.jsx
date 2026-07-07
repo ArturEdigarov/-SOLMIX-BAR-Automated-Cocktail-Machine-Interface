605f9f78e12d5c38612cf65e365e9e030c515a5c
Arthur Yediharov <arthuryediharov@soladmins-MacBook-Air.local>
2026-07-07 12:36:34 +0200

    camera fix

--- src/componnents/QrCodeDialog.jsx
+++ src/componnents/QrCodeDialog.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Dialog,
   DialogContent,
@@ -10,12 +10,18 @@ import {
 import { useZxing } from 'react-zxing';
 
 const QrCode = ({ open, onOpenChange, onScanSuccess }) => {
+    const [videoRef, setVideoRef] = useState(null);
     const { ref } = useZxing({
-        paused: !open,
+        paused: !open || !videoRef, // Ждем, пока videoRef не будет задан
+        constraints: { video: { facingMode: { ideal: "environment" } } },
         onDecodeResult(result) {
             onScanSuccess(result.getText());
         },
     });
+    const handleVideoRef = (element) => {
+        ref.current = element; // Привязываем к zxing
+        setVideoRef(element);  // Сообщаем React, что видео готово
+    };
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-md bg-white text-slate-900 rounded-3xl p-6">
@@ -24,11 +30,11 @@ const QrCode = ({ open, onOpenChange, onScanSuccess }) => {
         </DialogHeader>
         
         <div className="mt-4 overflow-hidden rounded-2xl border-2 border-dashed border-slate-700 ">
-          <video ref={ref} muted playsInline />
+          <video ref={handleVideoRef} muted playsInline className="w-full h-auto rounded-xl"/>
         </div>
       </DialogContent>
     </Dialog>
   )
 }
 
-export default QrCode
+export default QrCode;
